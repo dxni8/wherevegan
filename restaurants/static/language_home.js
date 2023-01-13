612ebd163c7ad7afa67f1acedd69917ea3ecc9a2
Daniel Wlach <daniel.wlach@gmx.at>
2023-01-13 12:28:30 +0100

    language css

--- restaurants/static/language_home.js
+++ restaurants/static/language_home.js
@@ -1,26 +1,12 @@
 document.addEventListener('DOMContentLoaded', function(){
     let language_counter = 0
     let counter_map = 0
-    document.getElementById('header-item-div').onclick = function(){
-        language_counter += 1
-        if(language_counter % 2 == 0){
-            // english
-            english()
-        } else {
-            // german
-            german()
-        }
-    }
     document.getElementById('mapButton').onclick = function(){
         counter_map = counter_map + 1;
         if(counter_map % 2 == 0){
             document.getElementById('map').style.display = 'none';
             document.getElementById('fill').style.display = 'none';
-            if(language_counter % 2 == 0){
-                document.getElementById('mapButton').innerHTML = 'map';
-            } else{
-                document.getElementById('mapButton').innerHTML = 'Karte';
-            }
+            document.getElementById('mapButton').innerHTML = 'map';
         } else{
             document.getElementById('map').style.display = 'block';
             document.getElementById('fill').style.display = 'block';
@@ -30,29 +16,6 @@ document.addEventListener('DOMContentLoaded', function(){
 })
 
 
-function english(){
-    document.getElementById('header-item-div').style.backgroundImage = 'url("/static/pics/german.png")'
-    document.querySelector('.heading').innerHTML = ' Find vegan<br/>restaurants in <br/>your area'
-    document.querySelector('.navbar-item-contact').innerHTML = 'contact'
-    document.querySelector('#navbar-item-last').innerHTML = 'recipes'
-    document.querySelector('#mybutton').value = 'search'
-
-    if(document.getElementById('mapButton').innerHTML == 'Karte'){
-        document.getElementById('mapButton').innerHTML = 'map'
-    }
-
-}
-function german(){
-    document.getElementById('header-item-div').style.backgroundImage = 'url("/static/pics/english.png")'
-    document.querySelector('.heading').innerHTML = 'Finde vegane<br/>Restaurants in <br/>deiner Nähe'
-
-    document.querySelector('.navbar-item-contact').innerHTML = 'Kontakt'
-    document.querySelector('#navbar-item-last').innerHTML = 'Rezepte'
-    document.querySelector('#mybutton').value = 'suchen'  
-    if(document.getElementById('mapButton').innerHTML == 'map'){
-        document.getElementById('mapButton').innerHTML = 'Karte'
-    }
-} 
 
 
 
